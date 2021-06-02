f6160dca43718059e977ea63f8b44d03a13c8d6c
ILIA003 <andronovily@gmail.com>
2021-06-02 19:20:29 +0300

    commit

--- launch.json/MINI_CALKULATOR.py
+++ launch.json/MINI_CALKULATOR.py
@@ -1,6 +1,6 @@
 a = input("Введите действие: ")
-b = int(input("Введите цифру "))
-d = int(input("Введите цифру "))
+b = int(input("Введите цифру: "))
+d = int(input("Введите цифру: "))
 if a == "+":
     print(b  +  d)
 elif a == "-":
