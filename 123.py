dbbb3eaffd622755d27d94391adef1d900f5d9ec
ILIA003 <andronovily@gmail.com>
2021-06-08 12:09:26 +0300

    commit

---
+++ 123.py
@@ -0,0 +1,560 @@
+a = input("Введите действие: ") # 1
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 2
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 3
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 4
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 5
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 6
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 7
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 8
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 9
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 10
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 11
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 12
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 13
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 14
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 15
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 16
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 17
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 18
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 19
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 20
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 21
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 22
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 23
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 24
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 25
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 26
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 27
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 28
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 29
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 30
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 31
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 32
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 33
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 34
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+
+a = input("Введите действие: ") # 35
+b = int(input("Введите цифры: "))
+c = int(input("Введите цифры: "))
+if a == "+":
+    print(b + c)
+elif a == "-":
+    print(b - c)
+elif a == "*":
+    print(b * c)
+elif b == 0 or c == 0: # elif идёт по порядку сверху вниз)
+    print("На ноль делить нельзя")
+elif a == "/":
+    print(b / c)
+else:
+    print("Неверный ввод")
+i = input()
